36aab6a3c863ee1d5f5d0b4abcea51f661d67bb2
Gentle-Jeffrey <geraldinose1@gmail.com>
2023-08-07 19:28:10 -0700

    all task done

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-Project on Static Libraries
+This is a readme for the 0x09-static_libraries_project.
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,3 +2,6 @@
 gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
+#!/bin/bash
+gcc -c *.c
+ar -rcs liball.a *.o
